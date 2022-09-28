1ce3d578f8823c4c7e32f7e455f8668b8401e7fd
ZalazarAlfonso <librosdrive11@gmail.com>
2022-09-28 19:00:54 +0100

    Correcion de container "quienes somos"
    Cambios en main para acomodar styles

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navBar = document.getElementsByClassName('navbar')[0]
 
 toggleButton.addEventListener('click', () => {
-    navBar.classList.toggle('active')
+    navBar.classList.toggle('active');
 })
