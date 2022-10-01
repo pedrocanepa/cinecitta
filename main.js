10e8f96e898aeb5d50a49e8447a2d8a1e6b1325d
ZalazarAlfonso <librosdrive11@gmail.com>
2022-10-01 12:12:40 +0100

    Cambios en Menu
    Cambios en el menu intermedio

--- main.js
+++ main.js
@@ -3,8 +3,4 @@ const navBar = document.getElementsByClassName('navbar')[0];
 
 toggleButton.addEventListener('click', () => {
     navBar.classList.toggle('active');
-<<<<<<< HEAD
-})
-=======
 });
->>>>>>> f135c5f767e7ff356e0cb04b66223b626f3659f8
