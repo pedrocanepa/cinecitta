c989a5edd5f6200ff15f25d0dcca4a3211046477
Pedro C <pedrocanepa441@gmail.com>
2022-10-02 23:24:43 -0300

    agregué accordions en las páginas de reseñas

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ const navBar = document.getElementsByClassName('navbar')[0];
 toggleButton.addEventListener('click', () => {
     navBar.classList.toggle('active');
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("accordionactive");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
---
+++ js/resenas.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navBar = document.getElementsByClassName('navbar')[0];
+
+toggleButton.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+});
