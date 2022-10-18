9dd4b5fde672310cf288ba2ffd56f89b487da70a
Pedro C <pedrocanepa441@gmail.com>
2022-10-18 00:05:51 -0300

    pruebas en registro.js

--- js/registro.js
+++ js/registro.js
@@ -1,68 +1,99 @@
-//Validación de un formulario con Javascript
-//Fuente: https://desarrolloweb.com/articulos/1767.php
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById('formregistro').addEventListener('submit', validarFormulario);
-});
+//Curso Básico de Javascript 22.- [Practica] Como validar un formulario sencillo
+//Fuente: https://www.youtube.com/watch?v=VLFBHnkC-vc
+(function () {
+  //Variables
+  var formregistro = document.getElementsByName('formregistro')[0],
+      elementos = formregistro.elements,
+      boton = document.getElementById('botonEnviar');
 
-function validarFormulario(event) {
-  event.preventDefault();
-  var apellido = document.getElementById('apellido').value;
-  if (apellido == 0) {
-    alert("Completa el campo Apellido");
-    return;
-  }
-  var nombre = document.getElementById('nombre').value;
-  if (nombre == 0) {
-    alert("Completa el campo Nombre");
-    return;
-  }
-  var email = document.getElementById('email').value;
-  if (email == 0) {
-    alert("Completa el campo Email");
-    return;
-  }
-  var cumpleanos = document.getElementById('cumpleanos').value;
-  if (cumpleanos == 0) {
-    alert("Completa el campo Cumpleaños");
-    return;
-  }
-  var genero = document.getElementById('genero').value;
-  if (genero == 0) {
-    alert("Completa el campo Género");
-    return;
-  }
-  var idioma = document.getElementById('idioma').selectedIndex;
-  if (idioma == 0) {
-    alert("Completa el campo Idioma");
-    return;
-  }
-  var redes = document.getElementsByName('redes').checked;
-  if (redes[0] == true  ||
-    redes[1] == true ||
-    redes[2] == true ||
-    redes[3] == true ||
-    redes[4] == true ||
-    redes[5] == true) {
-    alert("Completa el campo Redes");
-    return;
-  }
-  var password = document.getElementById('password').value;
-  if (password.length < 6) {
-    alert("La contraseña no es válida");
-    return;
-  }
-  var passwordCheck = document.getElementById('passwordConfirm').value;
-  if (passwordCheck != password) {
-    alert("Las contraseñas no coinciden");
-    return;
-  }
-  this.submit();
-}
+  var validarApellido = function (e) {
+    if (formregistro.apellido.value == 0) {
+      alert("Completa el campo Apellido");
+      e.preventDefault();
+      //https://www.w3schools.com/jsref/event_preventdefault.asp
+      //Evita que un enlace abra la URL
+    }
+  };
+
+  var validarNombre = function (e) {
+    if (formregistro.nombre.value == 0) {
+      alert("Completa el campo nombre");
+      e.preventDefault();
+    }
+  };
+
+  var validarEmail = function (e) {
+    if (formregistro.email.value == 0) {
+      alert("Completa el campo Email");
+      e.preventDefault();
+    }
+  };
+
+  var validarGenero = function (e) {
+    if (formregistro.genero.value == 0) {
+      alert("Completa el campo Género");
+      e.preventDefault();
+    }
+  };
+
+  var validarIdioma = function (e) {
+    if (formregistro.idioma.selectedIndex == 0) {
+      alert("Completa el campo Idioma");
+      e.preventDefault();
+    }
+  };
+
+  var validarCumpleanos = function (e) {
+    if (formregistro.cumpleanos.value == 0) {
+      alert("Completa el campo Cumpleaños");
+      e.preventDefault();
+    }
+  };
+
+  var validarRadio = function (e) {
+    if (formregistro.redes[0].checked == true ||
+      formregistro.redes[1].checked == true ||
+      formregistro.redes[2].checked == true ||
+      formregistro.redes[3].checked == true ||
+      formregistro.redes[4].checked == true ||
+      formregistro.redes[5].checked == true) {
+    } else {
+      alert("Completa el campo Redes");
+      e.preventDefault();
+    }
+  };
+
+  var validarPassword = function (e) {
+    if (formregistro.password.value == 0) {
+      alert("Completa el campo Contraseña");
+      e.preventDefault();
+    } else if (formregistro.passwordconfirm.value == 0) {
+      alert("Completa el campo Confirmar Contraseña");
+      e.preventDefault();
+    } else if (formregistro.password != formregistro.passwordconfirm) {
+      alert("Las contraseñas no coinciden");
+      e.preventDefault();
+    }
+  };
+
+  var validar = function (e) {
+    validarApellido(e);
+    validarNombre(e);
+    validarEmail(e);
+    validarGenero(e);
+    validarIdioma(e);
+    validarCumpleanos(e);
+    validarRadio(e);
+    validarPassword(e);
+  };
+
+  formregistro.addEventListener("submit", validar);
+}());
 
 
 // the form id is formregistro
-$('#formregistro').on("submit", function (event) {
-  event.preventDefault(); // prevent reload
+$('#formregistro').on('submit', function (e) {
+  e.preventDefault(); // prevent reload
 
   var formData = new FormData(formregistro);
   formData.append('service_id', 'default_service');
@@ -75,7 +106,7 @@ $('#formregistro').on("submit", function (event) {
     contentType: false, // auto-detection
     processData: false // no need to parse formData to string
   }).done(function () {
-    alert('Your mail is sent!');
+    alert('¡Gracias por registrarse!');
   }).fail(function (error) {
     alert('Oops... ' + JSON.stringify(error));
   });
---
+++ js/registro_test01.js
@@ -0,0 +1,6 @@
+const formregistro = document.getElementById('formregistro');
+const inputs = document.querySelectorAll('#formregistro input');
+
+const validarFormulario = (e) => {
+  
+}
